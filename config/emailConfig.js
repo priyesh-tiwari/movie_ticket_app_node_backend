cc107e458c1639371b956cb3bd33a9e78f66f5f8
Priyesh Tiwari <priyesh.garhwa@gmail.com>
2026-03-17 09:07:51 +0530

    fix: otp transporter

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer');
 
-const trnasporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
 });
+
+module.exports = transporter;
